f8cb4090eb887c8ff0b17a1a6b17878f09d01d91
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-22 17:29:39 +0100

    spostato stile

--- src/pages/Products/style.ts
+++ src/pages/Products/style.ts
@@ -1,25 +1,2 @@
 import styled from 'styled-components'
 
-
-export const Logo = styled.h1`
-    text-align: center;
-    margin: 40px 0;
-
-`;
-
-export const Box = styled.div`
-    display: flex;
-    justify-content: space-between;
-
-`;
-
-export const Line = styled.div `
-    width: 100%;
-    border-bottom: grey  1px solid;
-
-`;
-
-export const Title = styled.h3 `
-    color: #555555;
-
-`;
