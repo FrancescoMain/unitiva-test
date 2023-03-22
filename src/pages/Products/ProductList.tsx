bd45688cc34ed3be9391ea181e4af8a64bf1f91f
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-22 17:02:51 +0100

    importato componente navbar

--- src/pages/Products/ProductList.tsx
+++ src/pages/Products/ProductList.tsx
@@ -1,19 +1,23 @@
 import { useSelector } from "react-redux";
+import Navbar from "../../Components/Navbar/Navbar";
 import { selectProduct } from "./store/Product.selector";
 
 export const ProductList = () => {
   const products = useSelector(selectProduct);
 
   return (
-    <div>
-      <h1>Product List</h1>
-      {/* map di tutti i prodotti */}
-      {products.map((product) => (
-        <div key={product.id}>
-          <span>{product.title + product.Price}</span>
-        </div>
-      ))}
-    </div>
+    <>
+      <Navbar></Navbar>
+      <div>
+        <h1>Product List</h1>
+        {/* map di tutti i prodotti */}
+        {products.map((product) => (
+          <div key={product.id}>
+            <span>{product.title + product.Price}</span>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
