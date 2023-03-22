7478edd6a9ec145766d54706b0ae603fdcb90aa4
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-22 18:06:01 +0100

    creati imgContainer, image e box

--- src/pages/Products/style.ts
+++ src/pages/Products/style.ts
@@ -4,3 +4,20 @@ export const Wrap = styled.div`
     background-color:#f1f1f1;
 
 `;
+
+export const ImgContainer = styled.div`
+    width: calc(100% / 6);
+    
+
+`;
+
+export const Image = styled.img`
+    object-fit: cover;
+    width: 100%;
+
+`;
+
+export const Box = styled.div`  
+    display: flex;
+
+`;
