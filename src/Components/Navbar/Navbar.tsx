ade839486619943bb834eb20755fe8489dbf802f
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-22 17:22:11 +0100

    header dinamico

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Box, Line, Logo, Title } from "../../pages/Products/style";
+import { Page } from "../../style";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+interface Props {
+  title: string;
+  info: string;
+}
+
+const Navbar = ({ title, info }: Props) => {
+  return (
+    <Page>
+      <header>
+        <Logo>STORE LOGO</Logo>
+        <Box>
+          <Title>{title}</Title>
+          <span>{info}</span>
+        </Box>
+        <Line />
+      </header>
+    </Page>
+  );
 };
 
 export default Navbar;
