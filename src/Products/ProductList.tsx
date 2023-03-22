a08e01d1b94ec851032e8ef71ba2bde5f2aca433
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-22 16:56:17 +0100

    spostato type products

--- src/Products/ProductList.tsx
+++ src/Products/ProductList.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import { useSelector } from "react-redux";
-import { Product } from "../store/type";
 import { selectProduct } from "./store/Product.selector";
 
 export const ProductList = () => {
