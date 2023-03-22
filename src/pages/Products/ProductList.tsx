7dc2be033ea36b8e0b43ceb1ace3419804a514fb
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-22 17:46:32 +0100

    impaginazione tramite warp e show dei dati

--- src/pages/Products/ProductList.tsx
+++ src/pages/Products/ProductList.tsx
@@ -2,27 +2,35 @@ import { useSelector } from "react-redux";
 import Header from "../../Components/Header/Header";
 import { Page } from "../../style";
 import { selectProduct } from "./store/Product.selector";
+import { Wrap } from "./style";
 
 export const ProductList = () => {
   const products = useSelector(selectProduct);
 
   return (
     <>
-      <Page>
-        <Header
-          title="LAST PRODUCT AVAILABLE"
-          info="5 product available"
-        ></Header>
-        <div>
-          <h1>Product List</h1>
-          {/* map di tutti i prodotti */}
-          {products.map((product) => (
-            <div key={product.id}>
-              <span>{product.title + product.Price}</span>
-            </div>
-          ))}
-        </div>
-      </Page>
+      <Wrap>
+        <Page>
+          <Header
+            title="LAST PRODUCT AVAILABLE"
+            info="5 product available"
+          ></Header>
+          <div>
+            {/* map di tutti i prodotti */}
+            {products.map((product) => (
+              <div key={product.id}>
+                <div>
+                  <img src="" alt="" />
+                </div>
+                <h3>{product.title}</h3>
+                <span>{product.code}</span>
+                <span>Qty:{product.Qty}</span>
+                <span>Price:{product.Price}</span>
+              </div>
+            ))}
+          </div>
+        </Page>
+      </Wrap>
     </>
   );
 };
