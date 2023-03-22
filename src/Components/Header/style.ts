36443a0fd06af5cd92c852f8c6bb9da7e75839a2
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-22 17:29:14 +0100

    creato style header

---
+++ src/Components/Header/style.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+
+export const Logo = styled.h1`
+    text-align: center;
+    margin: 40px 0;
+
+`;
+
+export const Box = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+`;
+
+export const Line = styled.div `
+    width: 100%;
+    border-bottom: grey  1px solid;
+    margin-bottom: 10px;
+
+`;
+
+export const Title = styled.h3 `
+    color: #555555;
+
+`;
