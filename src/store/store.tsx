8bdf0ca1f01eea55fc2a7d8ccaf6fbcc5a9215f4
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-23 09:44:24 +0100

    riconfigurato store

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,12 +1,14 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { productReducer } from "../pages/Products/store/Product.reducer";
-
-const rootReducer = combineReducers({
-  products: productReducer,
-});
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { ProductSlice } from "../pages/Products/store/productsSlice";
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    product: ProductSlice.reducer,
+  },
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
+export const useAppDispatch: () => typeof store.dispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<
+  ReturnType<typeof store.getState>
+> = useSelector;
