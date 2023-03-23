7e8f3dd4521d949ac75326a0747ba6e42d5fa2f3
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-23 12:30:21 +0100

    modifica stile

--- src/pages/Products/style.ts
+++ src/pages/Products/style.ts
@@ -1,23 +1,86 @@
+import { Button, MenuItem, Select } from '@mui/material';
 import styled from 'styled-components'
 
 export const Wrap = styled.div`
     background-color:#f1f1f1;
+    border-bottom: 1px solid lightgrey;
+
 
 `;
 
 export const ImgContainer = styled.div`
-    width: calc(100% / 6);
-    
+    width:100%;
+    height: 100px;
+    overflow : hidden ;
+    border-bottom: 1px solid #c5c5c5;
 
 `;
 
 export const Image = styled.img`
     object-fit: cover;
+    height: 100%;
     width: 100%;
+    border-radius: 5% 5% 0 0;
 
 `;
 
 export const Box = styled.div`  
     display: flex;
 
+`;
+
+export const Card = styled.div`
+    width: calc(100% / 6);
+    margin: 10px;
+    border: 1px solid #c5c5c5;
+    border-radius: 5%;
+
+`;
+
+export const Title = styled.h3`
+    color: #6a6a6a
+`;
+
+export const Info = styled.div`
+    padding: 10px;
+
+`;
+
+export const Code = styled.span`
+    font-size: 12px;
+    display: block;
+
+`;
+
+export const Qty = styled.span`
+    display: block;
+    margin-top: 6px;
+    font-size: 12px;
+
+`;
+
+export const Price = styled.span`
+    display: block;
+    margin-top: 6px;
+    font-size: 12px;
+
+`;
+
+export const SelectSize = styled.select`
+    background-color: white;
+    border: 1px #b2b2b2 solid;
+    border-radius: 4px;
+    padding: 2px;
+    font-size: 10px;
+    width: 90px;
+    height: 20px;
+`;
+
+export const MyButton = styled(Button)`
+    margin-left: 40px !important;
+    height: 25px;
+    font-size: 12px !important;
+    text-transform: none !important;
+    background-color: #5f729d !important;
+    min-width: none !important;
 `;
