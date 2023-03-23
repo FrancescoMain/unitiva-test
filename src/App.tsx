b36727e512d8037343d55aad929d9f0403348f15
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-23 17:51:52 +0100

    importato router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import ProductList from "./pages/Products/ProductList";
+
+import { ReactNode } from "react";
+
+export interface Route {
+  path: string;
+  element: ReactNode;
+  name: string;
+}
+
+export const router: Route[] = [
+  {
+    path: "/",
+    element: <ProductList></ProductList>,
+    name: "ProductList",
+  },
+];
+
+export const routerController = createBrowserRouter(router);
 
 function App() {
   return (
     <div className="App">
       {/* //importato product list */}
-      <ProductList></ProductList>
+      <RouterProvider router={routerController} />
     </div>
   );
 }
