6d1e533405a1aadafa29b9c9e576624b944007ba
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-23 17:31:49 +0100

    imported cart

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { CartProduct } from "../../redux/cart/cartSlice";
+import { useAppSelector } from "../../redux/store";
 import { MyButton } from "../../style";
 import { Box } from "./style";
 
@@ -8,12 +10,23 @@ interface Props {
 }
 
 const Footer = ({ productsCounter, link }: Props) => {
+  const cart = useAppSelector((state) => state.cart);
+
   return (
     <Box>
       <div>
-        <span>{productsCounter}</span>
+        <span>
+          {cart.length}
+          {productsCounter}
+        </span>
       </div>
-      <MyButton size="large">{link}</MyButton>{" "}
+      <MyButton
+        color={cart.length === 0 ? "secondary" : "primary"}
+        disabled={cart.length === 0 ? true : false}
+        size="large"
+      >
+        {link}
+      </MyButton>{" "}
     </Box>
   );
 };
