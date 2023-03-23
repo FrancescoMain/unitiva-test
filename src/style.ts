8ab5c922f47227e413a004c2d3f1090706835409
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-23 16:52:42 +0100

    color props

--- src/style.ts
+++ src/style.ts
@@ -14,9 +14,8 @@ export const MyButton = styled(Button)`
 
     height: ${({ size }) => size === 'large' ? '40px' : '25px'};
     min-width: ${({ size }) => size === 'large' ? '120px' : 'none' }!important;
-
+    color: ${({ color  }) => color === "secondary" ? "lightgrey" : "white"} !important;
+    background-color: ${({color}) => color === "secondary" ? "#999999" : "#5f729d"}!important;
     font-size: 12px !important;
     text-transform: none !important;
-    background-color: #5f729d !important;
-    color: white !important;
 `;
