53e70fa40bc400b6668ccb3defc0214101f7bbe4
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-23 13:11:36 +0100

    static footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+import { MyButton } from "../../style";
+import { Box } from "./style";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box>
+      <div>
+        <span>0 product Added</span>
+      </div>
+      <MyButton size="large">Go To Cart</MyButton>{" "}
+    </Box>
+  );
 };
 
 export default Footer;
