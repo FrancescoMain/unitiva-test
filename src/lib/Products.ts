c72500e39178fb0cba02a26f39b68ff1a06d6e0a
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-23 12:29:42 +0100

    product styled component

---
+++ src/lib/Products.ts
@@ -0,0 +1,57 @@
+import { Product } from "./type";
+
+export const Products :Product[] =  [
+    {
+      id: 1,
+      title: "Ray-Ban Wayfarer",
+      code: "RB3210 2112A",
+      qty: 1,
+      price: 121.60,
+      image: "https://images.ray-ban.com/is/image/RayBan/805289126577_0001.png?impolicy=SEO_4x3",
+      sizes: [50, 52, 60],
+    },
+    {
+      id: 2,
+      title: "Ray-Ban Round",
+      code: "RB9210 1149",
+      qty: 1, 
+      price: 166.00, 
+      image: "https://images.ray-ban.com/is/image/RayBan/805289126577_0001.png?impolicy=SEO_4x3",
+      sizes: [50, 52, 60],
+
+    },
+    {
+      id: 3,
+      title: "Ray-Ban Clubmaster",
+      code: "RB3359 2112A",
+      qty: 3,
+      price: 152.00,
+      image: "https://images.ray-ban.com/is/image/RayBan/805289126577_0001.png?impolicy=SEO_4x3",
+      sizes: [50, 52, 60],
+
+
+    },
+    {
+      id: 4,
+      title: "Ray-Ban RB3594",
+      code: "RB3956 21122",
+      qty: 0,
+      price: 250.90,
+      image: "https://images.ray-ban.com/is/image/RayBan/805289126577_0001.png?impolicy=SEO_4x3",
+      sizes: [50, 52, 60],
+
+
+    },
+    {
+      id: 5,
+      title: "Ray-Ban Aviator",
+      code: "RB6211 2112A",
+      qty: 0,
+      price: 150.90,
+      image: "https://images.ray-ban.com/is/image/RayBan/805289126577_0001.png?impolicy=SEO_4x3",
+      sizes: [50, 52, 60],
+
+
+    },
+    
+  ];
