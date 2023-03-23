fe20d24113e72cd032f631676946d0d684325a86
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-23 13:11:48 +0100

    footer box style

---
+++ src/Components/Footer/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+
+export const Box = styled.div`
+    
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 30px;
+
+`;
