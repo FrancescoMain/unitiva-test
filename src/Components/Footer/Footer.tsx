ab8d902943a76b1e2b69e77ebfb0a10a12f8152c
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-24 16:06:19 +0100

    dinamicità link footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,36 +1,49 @@
-import styled from "styled-components";
-import { CartProduct } from "../../redux/cart/cartSlice";
 import { useAppSelector } from "../../redux/store";
 import { MyButton } from "../../style";
-import { Box } from "./style";
+import { Box, Wrap, Back } from "./style";
 import { useNavigate } from "react-router-dom";
-
 interface Props {
   productsCounter: string;
   link: string;
+  text: string;
+  backShow?: boolean;
 }
 
-const Footer = ({ productsCounter, link }: Props) => {
+const Footer = ({ productsCounter, link, text, backShow }: Props) => {
   const cart = useAppSelector((state) => state.cart);
   const navigate = useNavigate();
 
   return (
     <Box>
       <div>
-        <span>
-          {cart.length} {productsCounter}
-        </span>
+        {backShow && (
+          <Back
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Back
+          </Back>
+        )}
       </div>
-      <MyButton
-        color={cart.length === 0 ? "secondary" : "primary"}
-        disabled={cart.length === 0 ? true : false}
-        size="large"
-        onClick={() => {
-          navigate("/cart");
-        }}
-      >
-        {link}
-      </MyButton>{" "}
+
+      <Wrap>
+        <div>
+          <span>
+            {cart.length} {productsCounter}
+          </span>
+        </div>
+        <MyButton
+          color={cart.length === 0 ? "secondary" : "primary"}
+          disabled={cart.length === 0 ? true : false}
+          size="large"
+          onClick={() => {
+            navigate(link);
+          }}
+        >
+          {text}
+        </MyButton>{" "}
+      </Wrap>
     </Box>
   );
 };
