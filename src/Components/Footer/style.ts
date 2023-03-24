014563bddecdc6bbbd46abf70b45ab61544ae88c
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-24 16:06:43 +0100

    aggiunto stile front

--- src/Components/Footer/style.ts
+++ src/Components/Footer/style.ts
@@ -4,8 +4,22 @@ import styled from 'styled-components'
 export const Box = styled.div`
     
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between;
     align-items: center;
     padding: 30px;
+
+`;
+
+export const Wrap = styled.div`
+    display: flex;
+    align-items: center;
+
+`;
+
+export const Back = styled.button`
+    background-color: white;
+    border: none;
+    color: #a2a2a2;
+    cursor: pointer;
 
 `;
