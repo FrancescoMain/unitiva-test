2ff75d4d0ff302e93a221c8aeb928adf6f7a01d6
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-24 11:17:40 +0100

    aggiunto link al footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import { CartProduct } from "../../redux/cart/cartSlice";
 import { useAppSelector } from "../../redux/store";
 import { MyButton } from "../../style";
 import { Box } from "./style";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   productsCounter: string;
@@ -11,19 +12,22 @@ interface Props {
 
 const Footer = ({ productsCounter, link }: Props) => {
   const cart = useAppSelector((state) => state.cart);
+  const navigate = useNavigate();
 
   return (
     <Box>
       <div>
         <span>
-          {cart.length}
-          {productsCounter}
+          {cart.length} {productsCounter}
         </span>
       </div>
       <MyButton
         color={cart.length === 0 ? "secondary" : "primary"}
         disabled={cart.length === 0 ? true : false}
         size="large"
+        onClick={() => {
+          navigate("/cart");
+        }}
       >
         {link}
       </MyButton>{" "}
