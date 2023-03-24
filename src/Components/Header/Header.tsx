989c0d5abc6fd083469975f868c3f36a3c8b6a40
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-24 11:18:15 +0100

    aggiunta dinamicità al footer

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,13 +1,20 @@
+import { useAppSelector } from "../../redux/store";
 import { Box, Line, Logo, Title } from "./style";
 import { Props } from "./types";
 
 const Navbar = ({ title, info }: Props) => {
+  const cart = useAppSelector((state) => state.cart);
+
   return (
     <header>
       <Logo>STORE LOGO</Logo>
       <Box>
         <Title>{title}</Title>
-        <span>{info}</span>
+        <span>
+          {cart.length}
+
+          {info}
+        </span>
       </Box>
       <Line />
     </header>
