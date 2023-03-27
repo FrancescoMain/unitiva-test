fcd25414fb91c299045772f13e25a61d59d0f624
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-27 15:04:41 +0200

    stile checkout

---
+++ src/pages/checkout/style.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Box = styled.div`
+    height: 500px;
+    background-color: white;
+    border: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+`;
+export const TY = styled.h4`
+    font-size: 25px;
+    color: #555555;
+    margin-bottom: 30px;
+`;
+
+export const Title = styled.h3`
+    color: #555555;
+    font-size: 20px;
+    margin-bottom: 30px;
+
+`;
