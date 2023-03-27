17c04be86d1fbabf00ee192c52e788cfaf0e5124
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-27 15:04:19 +0200

    stile carrello ampliato

--- src/pages/cart/style.ts
+++ src/pages/cart/style.ts
@@ -47,7 +47,7 @@ interface TDProps {
 export const TD = styled.td<TDProps>`
     padding: 20px 10px;
     border-left: ${props => props.bordo ? "" : "1px solid grey"};
-
+    position: relative;
 `;
 
 export const ShopBox = styled.div`
@@ -78,4 +78,38 @@ export const UnderLine = styled.div`
     position: relative;
     bottom: 4px;
 
+`;
+
+export const RemoveButton = styled.button `
+    border: none;
+    background-color: white;
+    color: #b95151;
+    cursor: pointer;
+    position: absolute;
+    right: 20px;
+
+
+`;
+
+export const Del = styled.span `
+    border: 1px #b95151 solid;
+    border-radius: 50%;
+    padding: 1px 3px;
+`;
+
+export const AddButton = styled.button `
+    border: none;
+    background-color: white;
+    color: #b95151;
+    cursor: pointer;
+    position: absolute;
+    right: 20px;
+    color: #8e9bb9;
+
+`;
+
+export const Add = styled.span `
+    border: 1px #8e9bb9 solid;
+    border-radius: 50%;
+    padding: 1px 3px;
 `;
